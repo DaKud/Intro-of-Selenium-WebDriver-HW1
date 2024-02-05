965949a99788d64a4e1eb1b9083d193aee880285
Da <106489968+DaKud@users.noreply.github.com>
2024-02-05 16:51:14 +0300

    Update MainPage.java

--- Selenium-main/src/main/java/practiceWithSelenium/sm2/MainPage.java
+++ Selenium-main/src/main/java/practiceWithSelenium/sm2/MainPage.java
@@ -4,9 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-/**
- * @params MainPage  - pattern PageObject -> главной страницы и создания поста;
- */
+
 public class MainPage {
     @FindBy(id = "create-btn")
     WebElement createNewPost;
